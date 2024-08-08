0fd608d505ace3474d7eb6784b32614c837ba060
Grace <gracelee9901@gmail.com>
2024-08-08 14:52:43 -0700

    Link HSK levels to respective screens from home

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,27 @@
 import { View, TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from 'expo-router';
 
 const RootLayout = () => {
+  const navigation = useNavigation();
+  
   return (
     <View>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('hskOne')}>
         <Text>HSK 1</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('hskTwo')}>
         <Text>HSK 2</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('hskThree')}>
         <Text>HSK 3</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('hskFour')}>
         <Text>HSK 4</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('hskFive')}>
         <Text>HSK 5</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('hskSix')}>
         <Text>HSK 6</Text>
       </TouchableOpacity>
     </View>
