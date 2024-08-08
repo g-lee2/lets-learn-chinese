253f46f3188661199054c7fccbdb5bcfbcfc0ec8
Grace <gracelee9901@gmail.com>
2024-08-08 14:32:54 -0700

    Create reusable VocabItem component for HSK word display

---
+++ components/VocabItem.jsx
@@ -0,0 +1,20 @@
+import { View, Text, Touchable, TouchableOpacity, FlatList } from 'react-native';
+
+const VocabItem = () => {
+  return (
+    <FlatList 
+      data={data} 
+      numColumns={5}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <Text>
+            {item.hanzi}
+          </Text>
+        </TouchableOpacity>
+      )}
+    />
+  )
+}
+
+export default VocabItem;
