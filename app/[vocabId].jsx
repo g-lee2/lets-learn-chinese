7c02de19ace65417ab886ea5f0bf6bbe59bbe655
Grace <gracelee9901@gmail.com>
2024-08-17 17:59:44 -0700

    Add toggle functionality to show/hide vocab details

--- app/[vocabId].jsx
+++ app/[vocabId].jsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 import { useLocalSearchParams, Stack } from 'expo-router';
 import WritingBoard from '../components/WritingBoard';
 
@@ -7,22 +7,34 @@ const VocabDetails = () => {
   const {vocabId, vocabHanzi, vocabPinyin, vocabTranslations} = useLocalSearchParams();
   const vocabTranslationsArray = vocabTranslations.split(",");
   
+  const [hideTranslations, setHideTranslations] = useState(false);
+  const [hideHanzi, setHideHanzi] = useState(false);
+  const [hidePinyin, setHidePinyin] = useState(false);
+
   return (
     <View>
       <Stack.Screen
         options={{
-          title: vocabHanzi,
+          title: hideHanzi ? vocabId : vocabHanzi,
         }}
       />
-      <Text>{vocabId}</Text>
-      <Text>{vocabHanzi}</Text>
-      <Text>{vocabPinyin}</Text>
+      <Text>{hideHanzi ? '' : vocabHanzi}</Text>
+      <TouchableOpacity onPress={() => setHideHanzi(prev => !prev)}>
+        <Text>{hideHanzi ? 'Show' : 'Hide'} Hanzi</Text>
+      </TouchableOpacity>
+      <Text>{hidePinyin ? '' : vocabPinyin}</Text>
+      <TouchableOpacity onPress={() => setHidePinyin(prev => !prev)}>
+        <Text>{hidePinyin ? 'Show' : 'Hide'} Pinyin</Text>
+      </TouchableOpacity>
       {vocabTranslationsArray.map((translation) => {
         return (
-          <Text key={translation}>{translation}</Text>
+          <Text key={translation}>{hideTranslations ? '' : translation}</Text>
         );
       })}
-      <WritingBoard character={vocabHanzi} />
+      <TouchableOpacity onPress={() => setHideTranslations(prev => !prev)}>
+        <Text>{hideTranslations ? 'Show' : 'Hide'} Definition</Text>
+      </TouchableOpacity>
+      <WritingBoard character={hideHanzi ? '' : vocabHanzi} />
     </View>
   )
 }
