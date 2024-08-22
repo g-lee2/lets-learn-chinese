48ddcf9de348c14e613f3f357ed710c00b272f26
Grace <gracelee9901@gmail.com>
2024-08-21 21:42:55 -0700

    Adjust sizing and spacing of elements

--- app/index.jsx
+++ app/index.jsx
@@ -65,114 +65,115 @@ const RootLayout = () => {
   };
 
   return (
-    <ScrollView>
+    <ScrollView className='bg-white'>
       <View className='mb-16'>
-        <View className="p-8 h-1/6 bg-customGreen rounded-lg shadow-md mx-5 my-8">
+        <View className="p-8 h-1/6 bg-customGreen rounded-2xl shadow-xl mx-5 my-6">
           <View className='flex-row justify-between items-center'>
-            <View className='flex-col'>
-              <Text className='text-white'>{streakData?.length} </Text>
-              <Text className='text-white'>
-                day streak
+            <View className='flex-col items-center'>
+              <Text className='text-white text-6xl'>{streakData?.length} </Text>
+              <Text className='text-white font-semibold'>
+                Day Streak
               </Text>
             </View>
             <View className='w-20 h-20 bg-customWhite rounded-full justify-center items-center'>
-              <FontAwesome5 name="fire-alt" size={48} color="white" />
+              <FontAwesome5 name="fire-alt" size={54} color="white" />
             </View>
           </View>
         </View>
-        <View className="p-6 bg-white rounded-lg shadow-md mx-5 my-2">
+        <Text className='mx-6 text-lg font-bold'>HSK Levels</Text>
+        <View className="p-4 bg-white rounded-xl shadow-lg mx-5 my-2">
           <TouchableOpacity onPress={() => router.push('hskOne')}>
             <View className='flex-row justify-between items-center'>
               <View className='flex-row items-center space-x-4'>
-                <View className='p-2 bg-customGreen rounded-lg'>
-                  <SimpleLineIcons name="book-open" size={24} color="white"/>
+                <View className='p-2 bg-customGreen rounded-xl w-14 h-14 justify-center items-center'>
+                  <SimpleLineIcons name="book-open" size={32} color="white"/>
                 </View>
-                <View className='flex-col'>
-                  <Text>Learn</Text>
-                  <Text>HSK 1</Text>
+                <View className='flex-col space-y-2'>
+                  <Text className='text-xs text-slate-600'>Learn</Text>
+                  <Text className='font-bold'>HSK 1</Text>
                 </View>
               </View>
-              <Text>{'>'}</Text>
+              <Text className='text-slate-400 text-xl'>{'>'}</Text>
             </View>
           </TouchableOpacity>
         </View>
-        <View className='p-6 bg-white rounded-lg shadow-md mx-5 my-2'>
+        <View className='p-4 bg-white rounded-xl shadow-lg mx-5 my-2'>
           <TouchableOpacity onPress={() => router.push('hskTwo')}>
           <View className='flex-row justify-between items-center'>
               <View className='flex-row items-center space-x-4'>
-                <View className='p-2 bg-customGreen rounded-lg'>
-                  <SimpleLineIcons name="book-open" size={24} color="white"/>
+                <View className='p-2 bg-customGreen rounded-xl w-14 h-14 justify-center items-center'>
+                  <SimpleLineIcons name="book-open" size={32} color="white"/>
                 </View>
-                <View className='flex-col'>
-                  <Text>Learn</Text>
-                  <Text>HSK 2</Text>
+                <View className='flex-col space-y-2'>
+                  <Text className='text-xs text-slate-600'>Learn</Text>
+                  <Text className='font-bold'>HSK 2</Text>
                 </View>
               </View>
-              <Text>{'>'}</Text>
+              <Text className='text-slate-400 text-xl'>{'>'}</Text>
             </View>
           </TouchableOpacity>
         </View>
-        <View className='p-6 bg-white rounded-lg shadow-md mx-5 my-2'>
+        <View className='p-4 bg-white rounded-xl shadow-lg mx-5 my-2'>
           <TouchableOpacity onPress={() => router.push('hskThree')}>
             <View className='flex-row justify-between items-center'>
               <View className='flex-row items-center space-x-4'>
-                <View className='p-2 bg-customGreen rounded-lg'>
-                  <SimpleLineIcons name="book-open" size={24} color="white"/>
+                <View className='p-2 bg-customGreen rounded-xl w-14 h-14 justify-center items-center'>
+                  <SimpleLineIcons name="book-open" size={32} color="white"/>
                 </View>
-                <View className='flex-col'>
-                  <Text>Learn</Text>
-                  <Text>HSK 3</Text>
+                <View className='flex-col space-y-2'>
+                  <Text className='text-xs text-slate-600'>Learn</Text>
+                  <Text className='font-bold'>HSK 3</Text>
                 </View>
               </View>
-              <Text>{'>'}</Text>
+              <Text className='text-slate-400 text-xl'>{'>'}</Text>
             </View>
           </TouchableOpacity>
         </View>
-        <View className='p-6 bg-white rounded-lg shadow-md mx-5 my-2'>
+        <View className='p-4 bg-white rounded-xl shadow-lg mx-5 my-2'>
           <TouchableOpacity onPress={() => router.push('hskFour')}>
             <View className='flex-row justify-between items-center'>
               <View className='flex-row items-center space-x-4'>
-                <View className='p-2 bg-customGreen rounded-lg'>
-                  <SimpleLineIcons name="book-open" size={24} color="white"/>
+                <View className='p-2 bg-customGreen rounded-xl w-14 h-14 justify-center items-center'>
+                  <SimpleLineIcons name="book-open" size={32} color="white"/>
                 </View>
-                <View className='flex-col'>
-                  <Text>Learn</Text>
-                  <Text>HSK 4</Text>
+                <View className='flex-col space-y-2'>
+                  <Text className='text-xs text-slate-600'>Learn</Text>
+                  <Text className='font-bold'>HSK 4</Text>
                 </View>
               </View>
-              <Text>{'>'}</Text>
+              <Text className='text-slate-400 text-xl'>{'>'}</Text>
             </View>
           </TouchableOpacity>
         </View>
-        <View className='p-6 bg-white rounded-lg shadow-md mx-5 my-2'>
+        <View className='p-4 bg-white rounded-xl shadow-lg mx-5 my-2'>
           <TouchableOpacity onPress={() => router.push('hskFive')}>
             <View className='flex-row justify-between items-center'>
               <View className='flex-row items-center space-x-4'>
-                <View className='p-2 bg-customGreen rounded-lg'>
-                  <SimpleLineIcons name="book-open" size={24} color="white"/>
+                <View className='p-2 bg-customGreen rounded-xl w-14 h-14 justify-center items-center'>
+                  <SimpleLineIcons name="book-open" size={32} color="white"/>
                 </View>
-                <View className='flex-col'>
-                  <Text>Learn</Text>
-                  <Text>HSK 5</Text>
+                <View className='flex-col space-y-2'>
+                  <Text className='text-xs text-slate-600'>Learn</Text>
+                  <Text className='font-bold'>HSK 5</Text>
                 </View>
               </View>
-              <Text>{'>'}</Text>
+              <Text className='text-slate-400 text-xl'>{'>'}</Text>
             </View>
           </TouchableOpacity>
         </View>
-        <View className='p-6 bg-white rounded-lg shadow-md mx-5 my-2'>
+        <View className='p-4 bg-white rounded-xl shadow-lg mx-5 my-2'>
           <TouchableOpacity onPress={() => router.push('hskSix')}>
             <View className='flex-row justify-between items-center'>
               <View className='flex-row items-center space-x-4'>
-                <View className='p-2 bg-customGreen rounded-lg'>
-                  <SimpleLineIcons name="book-open" size={24} color="white"/>
+                <View className='p-2 bg-customGreen rounded-xl w-14 h-14 justify-center items-center'>
+                  <SimpleLineIcons name="book-open" size={32} color="white"/>
                 </View>
-                <View className='flex-col'>
-                  <Text>Learn</Text>
-                  <Text>HSK 6</Text>
+                <View className='flex-col space-y-2'>
+                  <Text className='text-xs text-slate-600'>Learn</Text>
+                  <Text className='font-bold'>HSK 6</Text>
                 </View>
               </View>
-              <Text>{'>'}</Text>
+              <Text className='text-slate-400 text-xl'>{'>'}</Text>
             </View>
           </TouchableOpacity>
         </View>
